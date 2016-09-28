bedd86bd81173f460577f0434fd4a07a9fb8ad7c
minodisk <daisuke.mino@gmail.com>
2016-09-28 12:51:15 +0900

    Add converting string methods to helper

--- helpers/general.go
+++ helpers/general.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"unicode"
 
 	schema "github.com/lestrrat/go-jsschema"
 )
@@ -21,14 +22,94 @@ func NotLast(i, len int) bool {
 
 func GetKeyFromJSONPath(url string) string {
 	s := strings.Split(url, "/")
-	return SnakeToUpperCamelCase(s[len(s)-1])
+	return UpperCamelCase(s[len(s)-1])
 }
 
 func LinkTitle(title, method, suffix string) string {
 	return method + SpaceToUpperCamelCase(title) + suffix
 }
 
+func isAlphabetical(c rune) bool {
+	return isLower(c) || isUpper(c)
+}
+
+func isUpper(c rune) bool {
+	return 'A' <= c && 'Z' >= c
+}
+
+func isLower(c rune) bool {
+	return 'a' <= c && 'z' >= c
+}
+
+func LowerSnakeCase(s string) string {
+	return strings.ToLower(SnakeCase(s))
+}
+
+func UpperSnakeCase(s string) string {
+	return strings.ToUpper(SnakeCase(s))
+}
+
+func SnakeCase(s string) string {
+	return Chop(s, '_')
+}
+
+func Chop(s string, d rune) string {
+	var b bytes.Buffer
+	for i, c := range s {
+		switch {
+		case isUpper(c):
+			if i > 0 {
+				p := rune(s[i-1])
+				if isLower(p) {
+					b.WriteRune(d)
+				}
+			}
+			b.WriteRune(c)
+		case !isAlphabetical(c):
+			b.WriteRune(d)
+		default:
+			b.WriteRune(c)
+		}
+	}
+	return b.String()
+}
+
+type convert func(rune) rune
+
+func LowerCamelCase(s string) string {
+	return CamelCase(s, unicode.ToLower)
+}
+
+func UpperCamelCase(s string) string {
+	return CamelCase(s, unicode.ToUpper)
+}
+
+func CamelCase(s string, fn convert) string {
+	var b bytes.Buffer
+	first := true
+	apply := false
+	for i := 0; i < len(s); i++ {
+		c := rune(s[i])
+		switch {
+		case !isAlphabetical(c):
+			apply = true
+		case first:
+			first = false
+			apply = false
+			b.WriteRune(fn(c))
+		case apply:
+			apply = false
+			b.WriteRune(unicode.ToUpper(c))
+		default:
+			apply = false
+			b.WriteRune(c)
+		}
+	}
+	return b.String()
+}
+
 func SpaceToUpperCamelCase(s string) string {
+	fmt.Printf("Warning: helpers.SpaceToUpperCamelCase is deprecated\n")
 	if s == "" {
 		return ""
 	}
@@ -41,6 +122,7 @@ func SpaceToUpperCamelCase(s string) string {
 }
 
 func SnakeToUpperCamelCase(s string) string {
+	fmt.Printf("Warning: helpers.SnakeToUpperCamelCase is deprecated\n")
 	if s == "" {
 		return ""
 	}
@@ -53,6 +135,7 @@ func SnakeToUpperCamelCase(s string) string {
 }
 
 func SnakeToLowerCamelCase(s string) string {
+	fmt.Printf("Warning: helpers.SnakeToLowerCamelCase is deprecated\n")
 	if s == "" {
 		return ""
 	}
@@ -69,6 +152,7 @@ func SnakeToLowerCamelCase(s string) string {
 }
 
 func ToUpperFirst(s string) string {
+	fmt.Printf("Warning: helpers.ToUpperFirst is deprecated\n")
 	buf := bytes.Buffer{}
 	buf.WriteString(strings.ToUpper(s[:1]))
 	buf.WriteString(s[1:])
@@ -76,6 +160,7 @@ func ToUpperFirst(s string) string {
 }
 
 func ToLowerFirst(s string) string {
+	fmt.Printf("Warning: helpers.ToLowerFirst is deprecated\n")
 	buf := bytes.Buffer{}
 	buf.WriteString(strings.ToLower(s[:1]))
 	buf.WriteString(s[1:])
